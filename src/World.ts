cf21185cf7a2656b0b24d61ad22e6cba1c3bf761
Gabriël Pouw <gabriel@pouw.com>
2023-02-17 15:24:55 +0100

    [CHANGED] Give the vertices of the plane a color depending on the heightmap

--- src/World.ts
+++ src/World.ts
@@ -1,7 +1,6 @@
 import * as THREE from "three";
 import { Mesh, Vector3 } from "three";
 import NoiseGenerator, { NoiseParams } from "./Noise"
-import TerrianTypes from "./TerrianTypes";
 import { degToRad } from "./Utisl";
 
 interface TerrianType {
@@ -45,44 +44,103 @@ export default class World{
         //Create some terriantypes
         this._terrianTypes = new Array; 
         this._terrianTypes.push(
-            { name: "Water", height: -1, color: 0xFF0000},
-            { name: "Land", height: 0, color: 0x0000FF}
+            { name: "Water", height: -15.0, color: 0xFF0000},
+            { name: "Land", height: 1.0, color: 0x0000FF}
         );
-        console.log(this._terrianTypes)
 
-        let normalMaterial = new THREE.MeshNormalMaterial({
+        const texture = new THREE.TextureLoader().load('./assets/land.jpg');
+
+        //Later maybe have different materials for different terrian types?
+        let phongMaterial = new THREE.MeshPhongMaterial({
             wireframe: false,
-            side: THREE.DoubleSide
+            color: 0x808080,
+            side: THREE.DoubleSide,
+            //vertexColors: true
+            map: texture
         });
-        let basicMaterial = new THREE.MeshBasicMaterial({
-            wireframe: false,
-            color: 0x80808080,
-            side: THREE.DoubleSide
-        });
+        phongMaterial.flatShading = true;
 
         let resolution = 55;
         let chunkSize = 100;
         this._chunk = new THREE.Mesh(
             new THREE.PlaneGeometry(chunkSize, chunkSize, resolution, resolution),
-            normalMaterial
+            phongMaterial
             );
         this._chunk.castShadow = false;
         this._chunk.receiveShadow = true;
+        this._chunk.position.set(0,0,0);
         
         this.ApplyHeightMap();
+        this.ApplyTerrianMap();
+
+        console.log(this._chunk);
         
         this._scene.add( this._chunk );
     }
 
+    //Offset the vertices in the z
     private ApplyHeightMap(){
+        //@ts-ignore
         let vertices = this._chunk.geometry.attributes.position["array"];
 
-        for(let i = 0; i < vertices.length; i += 3){
-            vertices[i + 2] = this._noise.Get(vertices[i] + this._chunk.position.x, vertices[i + 1] + this._chunk.position.z);
+        // +1 because this is what three js always does
+        let width = this._chunk.geometry.parameters.widthSegments + 1;
+        let height = this._chunk.geometry.parameters.heightSegments + 1;
+
+        for(let y = 0; y < height; y++){
+            for (let x = 0; x < width; x++) {
+
+                //We need to add 3 since each vertex is
+                //(x, y, z)
+                let index = (y * width + x) * 3;
+
+                //@ts-ignore
+                vertices[index + 2] = this._noise.Get(vertices[index] + this._chunk.position.x, vertices[index + 1] + this._chunk.position.z);
+            }
         }
         
         //Recalculate the normals
         this._chunk.geometry.computeVertexNormals();
+    }
+
+    private ApplyTerrianMap(){
+        // create a buffer with color dataw
+        let width = this._chunk.geometry.parameters.widthSegments + 1;
+        let height = this._chunk.geometry.parameters.heightSegments + 1;
+
+        const size = width * height;
+        const data = new Uint8Array( 4 * size );
+
+        let vertices = this._chunk.geometry.attributes.position["array"];
+        for(let y = 0; y < height; y++)
+        {
+            for(let x = 0; x < width; x++)
+            {
+                const index = (y * width + x);
+                const stride = index * 4;
+                const vertexIndex = index * 3;
+
+                let water = this._terrianTypes.find(terrianType => terrianType.name == "Water");
+                if(vertices[vertexIndex + 2] < water.height){
+                    data[ stride ] = 0;
+                    data[ stride + 1 ] = 0;
+                    data[ stride + 2 ] = 255;
+                    data[ stride + 3 ] = 255;
+                }
+                else{
+                    data[ stride ] = 0;
+                    data[ stride + 1 ] = 255;
+                    data[ stride + 2 ] = 0;
+                    data[ stride + 3 ] = 255;
+                }
+            }
+        }
+
+        // used the buffer to create a DataTexture
+        const texture = new THREE.DataTexture( data, width, height, THREE.RGBAFormat );
+        texture.needsUpdate = true;
+
+        this._chunk.material.map = texture;
     }
 
     private Lighthing(){
@@ -95,6 +153,6 @@ export default class World{
     }
 
     public updateEntities(deltaTime: number){
-        this._chunk.rotation.z += deltaTime * degToRad(25);
+        //this._chunk.rotation.z += deltaTime * degToRad(25);
     }
 }
